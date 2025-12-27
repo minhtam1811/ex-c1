4deb57db5fca2c54a401c6ba70854a7891ae2a84
minhtam1811 <163244119+minhtam1811@users.noreply.github.com>
2025-12-27 21:12:28 +0700

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,6 @@
+'''Given a list and a value entered via the keyboard,
+remove all instances of the value from the list'''
+
+numbers = [1,2,3,4,8,4,6,3,2,7,8,5,6]
+for i in range (len(numbers)-1,-1,-1):
+    print(numbers[i], end=''
