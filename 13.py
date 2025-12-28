0e379b6b08c84f66b0a581bf5dd7977a5b0371bd
minhtam1811 <163244119+minhtam1811@users.noreply.github.com>
2025-12-28 14:00:52 +0700

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,5 +1,5 @@
-'''Given a list and a value entered via the keyboard,
-remove all instances of the value from the list'''
+'''Given two lists, A and B, 
+find and output their intersection (common elements).'''
 
 x = [1,2,3,4,8,4,6,3,2,7,8,5,6]
 y = [1,5,7,3,7,3,8]
